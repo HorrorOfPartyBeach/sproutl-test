98e8c66e0c90c3f940152dd2ae1edd64f5c37c01
emmagilmour <emma.gilmour@ao.com>
2022-02-04 19:59:03 +0000

    feat: fetch blog titles from api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const url = "https://jsonplaceholder.typicode.com/posts";
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json, "JSON");
+        setPosts(json);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Sproutl Blog</header>
+      <main>
+        {posts.map((post) => {
+          return <div key={post.id}>{post.title}</div>;
+        })}
+      </main>
     </div>
   );
 }
